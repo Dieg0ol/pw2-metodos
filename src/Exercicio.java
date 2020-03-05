730dfe20e236395627f026372772c8b8316550f0
Diego <diegocaboclo@hotmail.com>
2020-03-04 22:31:10 -0300

    exercicio concluido

---
+++ src/Exercicio.java
@@ -0,0 +1,68 @@
+
+public class Exercicio {
+
+	public static int soma(int numero1, int numero2 ) {
+		return (numero1+numero2);
+	}
+	public static int proximo(int numero1 )  {
+		return (numero1 +1);
+	}
+	public static int dobro(int numero1) {
+		return (numero1*2);
+	}
+	public static void main(String[] args) {
+		int somar = soma(20, 3);
+		System.out.println("a soma é: " +somar);
+		int seguinte = proximo(20);
+		System.out.println("o numero seguinte é: "+seguinte);
+		int doubro = dobro(20);
+		System.out.println("o dobro do numero é: "+ doubro);
+
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
